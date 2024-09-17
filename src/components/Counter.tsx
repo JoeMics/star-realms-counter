e578ec9ae20660d55356a3d1094703ce6bb1d80b
JoeMics <46177831+JoeMics@users.noreply.github.com>
2024-09-17 12:43:09 -0700

    counter: add increments and decrements of 3 and 5 for authority

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -7,28 +7,35 @@ import AuthorityBar from '@/components/AuthorityBar';
 import CombatBar from '@/components/CombatBar';
 import TradeBar from '@/components/TradeBar';
 
-// Props: title, color
 export default function Counter() {
-  const [startingAuthority, setStartingAuthority] = useState(50);
+  const startingAuthority = 50;
+
   const [authority, setAuthority] = useState(startingAuthority);
   const [authorityModifier, setAuthorityModifier] = useState(0);
   const [combat, setCombat] = useState(0);
   const [trade, setTrade] = useState(0);
 
-  function decrementOnce(number: number) {
-    return number > 0 ? number - 1 : number;
-  }
-
-  function decrementHealth() {
-    if (authority > 0) {
-      setAuthority(decrementOnce(authority));
-      setAuthorityModifier(authorityModifier - 1);
+  /**
+   * Returns value of a counter after being reduced by a certain amount.
+   * Prevents the returned value from going into the negative
+   */
+  function decrementAmount(amount: number, currentCounterAmount: number) {
+    if (currentCounterAmount < amount) {
+      return 0;
     }
+
+    return currentCounterAmount - amount;
   }
 
-  function incrementHealth() {
-    setAuthority(authority + 1);
-    setAuthorityModifier(authorityModifier + 1);
+  function decrementHealth(amount: number) {
+    const newAmount = decrementAmount(amount, authority);
+    setAuthority(newAmount);
+    setAuthorityModifier(newAmount);
+  }
+
+  function incrementHealth(amount: number) {
+    setAuthority(authority + amount);
+    setAuthorityModifier(authorityModifier + amount);
   }
 
   function finishTurn() {
@@ -40,25 +47,57 @@ export default function Counter() {
   return (
     <>
       <div className='mx-auto my-0 flex items-center justify-center'>
-        <Button
-          variant='ghost'
-          size='icon'
-          onClick={() => decrementHealth()}
-        >
-          <CircleChevronLeftIcon className='h-8 w-8' />
-        </Button>
+        <div className='flex flex-col'>
+          <Button
+            variant='outline'
+            className='border-2 text-lg'
+            onClick={() => decrementHealth(5)}
+          >
+            -5
+          </Button>
+          <Button
+            variant='outline'
+            className='my-4 border-2 text-lg'
+            onClick={() => decrementHealth(3)}
+          >
+            -3
+          </Button>
+          <Button
+            variant='outline'
+            className='border-2 text-lg font-extrabold'
+            onClick={() => decrementHealth(1)}
+          >
+            -1
+          </Button>
+        </div>
         <AuthorityBar
           authority={authority}
           startingAuthority={startingAuthority}
           authorityModifier={authorityModifier}
         ></AuthorityBar>
-        <Button
-          variant='ghost'
-          size='icon'
-          onClick={() => incrementHealth()}
-        >
-          <CircleChevronRightIcon className='h-8 w-8' />
-        </Button>
+        <div className='flex flex-col'>
+          <Button
+            variant='outline'
+            className='border-2 text-lg'
+            onClick={() => incrementHealth(5)}
+          >
+            +5
+          </Button>
+          <Button
+            variant='outline'
+            className='my-4 border-2 text-lg'
+            onClick={() => incrementHealth(3)}
+          >
+            +3
+          </Button>
+          <Button
+            variant='outline'
+            className='border-2 text-lg'
+            onClick={() => incrementHealth(1)}
+          >
+            +1
+          </Button>
+        </div>
       </div>
 
       <Button
@@ -73,7 +112,7 @@ export default function Counter() {
         <Button
           variant='ghost'
           size='icon'
-          onClick={() => setCombat(decrementOnce(combat))}
+          onClick={() => setCombat(decrementAmount(1, combat))}
         >
           <CircleChevronLeftIcon className='h-8 w-8' />
         </Button>
@@ -91,7 +130,7 @@ export default function Counter() {
         <Button
           variant='ghost'
           size='icon'
-          onClick={() => setTrade(decrementOnce(trade))}
+          onClick={() => setTrade(decrementAmount(1, trade))}
         >
           <CircleChevronLeftIcon className='h-8 w-8' />
         </Button>
