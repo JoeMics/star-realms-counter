13779605b97696388653d335d361aab3839bde94
JoeMics <46177831+JoeMics@users.noreply.github.com>
2024-09-29 18:21:11 -0700

    minor: prevent mobile scroll refresh

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { CircleChevronLeftIcon, CircleChevronRightIcon } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 import AuthorityBar from '@/components/AuthorityBar';
 import CombatBar from '@/components/CombatBar';
