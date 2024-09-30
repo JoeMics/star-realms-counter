b15bde313415c4f28dedcd2aff81a620162aa46e
JoeMics <46177831+JoeMics@users.noreply.github.com>
2024-09-29 23:14:07 -0700

    feature: settings menu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,221 @@
-import Counter from '@/components/Counter';
+'use client';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
+import AuthorityBar from '@/components/AuthorityBar';
+import CombatBar from '@/components/CombatBar';
+import TradeBar from '@/components/TradeBar';
 import Header from '@/components/Header';
 
 export default function Home() {
+  const [startingAuthority, setStartingAuthority] = useState(50);
+  const [authority, setAuthority] = useState(startingAuthority);
+  const [authorityModifier, setAuthorityModifier] = useState(0);
+  const [combat, setCombat] = useState(0);
+  const [trade, setTrade] = useState(0);
+
+  /**
+   * Returns value of a counter after being reduced by a certain amount.
+   * Prevents the returned value from going into the negative
+   */
+  function decrementAmount(amount: number, currentCounterAmount: number) {
+    if (currentCounterAmount < amount) {
+      return 0;
+    }
+
+    return currentCounterAmount - amount;
+  }
+
+  function decrementHealth(amount: number) {
+    const newAmount = decrementAmount(amount, authority);
+    setAuthority(newAmount);
+    setAuthorityModifier(authorityModifier - (authority - newAmount));
+  }
+
+  function incrementHealth(amount: number) {
+    setAuthority(authority + amount);
+    setAuthorityModifier(authorityModifier + amount);
+  }
+
+  function finishTurn() {
+    setAuthorityModifier(0);
+    setCombat(0);
+    setTrade(0);
+  }
+
+  function resetGame() {
+    setAuthority(startingAuthority);
+    finishTurn();
+  }
+
   return (
     <>
-      <Header />
+      <Header
+        setStartingAuthority={setStartingAuthority}
+        startingAuthority={startingAuthority}
+        onResetClick={resetGame}
+      />
       <main className='row-start-2 flex flex-col items-center justify-items-center gap-8'>
-        <Counter />
+        <div className='mx-auto my-0 flex items-center justify-center'>
+          <div className='flex flex-col'>
+            <Button
+              variant='outline'
+              className='border-2 text-lg'
+              onClick={() => decrementHealth(5)}
+            >
+              -5
+            </Button>
+            <Button
+              variant='outline'
+              className='my-4 border-2 text-lg'
+              onClick={() => decrementHealth(3)}
+            >
+              -3
+            </Button>
+            <Button
+              variant='outline'
+              className='border-2 text-lg font-extrabold'
+              onClick={() => decrementHealth(1)}
+            >
+              -1
+            </Button>
+          </div>
+          <AuthorityBar
+            authority={authority}
+            startingAuthority={startingAuthority}
+            authorityModifier={authorityModifier}
+          ></AuthorityBar>
+          <div className='flex flex-col'>
+            <Button
+              variant='outline'
+              className='border-2 text-lg'
+              onClick={() => incrementHealth(5)}
+            >
+              +5
+            </Button>
+            <Button
+              variant='outline'
+              className='my-4 border-2 text-lg'
+              onClick={() => incrementHealth(3)}
+            >
+              +3
+            </Button>
+            <Button
+              variant='outline'
+              className='border-2 text-lg'
+              onClick={() => incrementHealth(1)}
+            >
+              +1
+            </Button>
+          </div>
+        </div>
+
+        <Button
+          className='mx-auto my-0'
+          variant={'outline'}
+          onClick={finishTurn}
+        >
+          Finish Turn
+        </Button>
+
+        <div className='mx-auto my-0 flex items-center justify-center'>
+          <div className='flex flex-col'>
+            <Button
+              variant='outline'
+              className='border-2 text-lg'
+              onClick={() => setCombat(decrementAmount(5, combat))}
+            >
+              -5
+            </Button>
+            <Button
+              variant='outline'
+              className='my-4 border-2 text-lg'
+              onClick={() => setCombat(decrementAmount(3, combat))}
+            >
+              -3
+            </Button>
+            <Button
+              variant='outline'
+              className='border-2 text-lg font-extrabold'
+              onClick={() => setCombat(decrementAmount(1, combat))}
+            >
+              -1
+            </Button>
+          </div>
+          <CombatBar combat={combat}></CombatBar>
+          <div className='flex flex-col'>
+            <Button
+              variant='outline'
+              className='border-2 text-lg'
+              onClick={() => setCombat(combat + 5)}
+            >
+              +5
+            </Button>
+            <Button
+              variant='outline'
+              className='my-4 border-2 text-lg'
+              onClick={() => setCombat(combat + 3)}
+            >
+              +3
+            </Button>
+            <Button
+              variant='outline'
+              className='border-2 text-lg'
+              onClick={() => setCombat(combat + 1)}
+            >
+              +1
+            </Button>
+          </div>
+        </div>
+
+        <div className='mx-auto my-0 flex items-center justify-center'>
+          <div className='flex flex-col'>
+            <Button
+              variant='outline'
+              className='border-2 text-lg'
+              onClick={() => setTrade(decrementAmount(5, trade))}
+            >
+              -5
+            </Button>
+            <Button
+              variant='outline'
+              className='my-4 border-2 text-lg'
+              onClick={() => setTrade(decrementAmount(3, trade))}
+            >
+              -3
+            </Button>
+            <Button
+              variant='outline'
+              className='border-2 text-lg font-extrabold'
+              onClick={() => setTrade(decrementAmount(1, trade))}
+            >
+              -1
+            </Button>
+          </div>
+          <TradeBar trade={trade}></TradeBar>
+          <div className='flex flex-col'>
+            <Button
+              variant='outline'
+              className='border-2 text-lg'
+              onClick={() => setTrade(trade + 5)}
+            >
+              +5
+            </Button>
+            <Button
+              variant='outline'
+              className='my-4 border-2 text-lg'
+              onClick={() => setTrade(trade + 3)}
+            >
+              +3
+            </Button>
+            <Button
+              variant='outline'
+              className='border-2 text-lg font-extrabold'
+              onClick={() => setTrade(trade + 1)}
+            >
+              +1
+            </Button>
+          </div>
+        </div>
       </main>
     </>
   );
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,34 @@
 'use client';
 
 import { Button } from '@/components/ui/button';
-import { Fullscreen, Settings } from 'lucide-react';
-import { useEffect, useState } from 'react';
+import {
+  Dialog,
+  DialogTrigger,
+  DialogContent,
+  DialogHeader,
+  DialogFooter,
+} from './ui/dialog';
+import { Input } from './ui/input';
+import { z } from 'zod';
+import { Fullscreen, Rocket, Settings } from 'lucide-react';
+import { MouseEventHandler, useEffect, useState } from 'react';
+import { DialogTitle } from '@radix-ui/react-dialog';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from './ui/form';
 
-export default function Header() {
+export default function Header(props: {
+  startingAuthority: number;
+  setStartingAuthority: React.Dispatch<React.SetStateAction<number>>;
+  onResetClick: MouseEventHandler<HTMLButtonElement>;
+}) {
   const [isFullScreen, setIsFullScreen] = useState(false);
 
   useEffect(() => {
@@ -26,9 +50,38 @@ export default function Header() {
     return document.exitFullscreen();
   }
 
+  // Form logic
+  const [open, setOpen] = useState(false);
+  const formSchema = z.object({
+    startingAuthority: z.number().min(1, {
+      message: 'Starting authority must be a value of 1 or greater.',
+    }),
+  });
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      startingAuthority: props.startingAuthority,
+    },
+  });
+
+  function onFormSubmit(values: z.infer<typeof formSchema>) {
+    props.setStartingAuthority(values.startingAuthority);
+  }
+
+  function onReset(event: React.MouseEvent<HTMLButtonElement>) {
+    props.onResetClick(event);
+    setOpen(false);
+  }
+
   return (
     <nav className='flex justify-between px-4 py-2'>
-      <h1 className='text-center text-2xl font-extrabold'>Star Realms</h1>
+      <div className='flex items-center py-2'>
+        <h1 className='hidden px-2 text-2xl font-extrabold md:block'>
+          Star Realms
+        </h1>
+        <Rocket className='text-center' />
+      </div>
       <div>
         <Button
           variant={'ghost'}
@@ -37,12 +90,59 @@ export default function Header() {
         >
           <Fullscreen />
         </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
+        <Dialog
+          open={open}
+          onOpenChange={setOpen}
         >
-          <Settings />
-        </Button>
+          <DialogTrigger asChild>
+            <Button
+              variant={'ghost'}
+              size={'icon'}
+            >
+              <Settings />
+            </Button>
+          </DialogTrigger>
+          <DialogContent className='sm:max-w-"[500px]'>
+            <DialogHeader>
+              <DialogTitle>Settings</DialogTitle>
+            </DialogHeader>
+            <Form {...form}>
+              <form
+                onSubmit={form.handleSubmit(onFormSubmit)}
+                className='space-y-8'
+              >
+                <FormField
+                  control={form.control}
+                  name='startingAuthority'
+                  render={({ field }) => (
+                    <FormItem>
+                      <FormLabel> Starting Authority </FormLabel>
+                      <FormControl>
+                        <Input
+                          {...field}
+                          type='number'
+                          onChange={(event) =>
+                            field.onChange(+event.target.value || '')
+                          }
+                        />
+                      </FormControl>
+                      <FormMessage />
+                    </FormItem>
+                  )}
+                />
+                <DialogFooter>
+                  <Button
+                    variant={'outline'}
+                    onClick={onReset}
+                  >
+                    New Game
+                  </Button>
+                  <Button type='submit'>Save changes</Button>
+                </DialogFooter>
+              </form>
+            </Form>
+          </DialogContent>
+        </Dialog>
       </div>
     </nav>
   );
